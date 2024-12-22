f3b2ea75953a8796fbd64571e796debaf72c15f8
s Day <dayss1224@gmail.com>
2024-12-22 17:33:02 +0800

    sync

--- astro.config.mjs
+++ astro.config.mjs
@@ -21,8 +21,8 @@ import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://fuwari.vercel.app/",
-  base: "/",
+  site: "https://Scudays.github.io",
+  base: "Astro-fuwari",
   trailingSlash: "always",
   integrations: [
     tailwind(
